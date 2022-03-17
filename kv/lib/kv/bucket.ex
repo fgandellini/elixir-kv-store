87f34072f98ef90e045c0adf047f110cdf5115a4
Michele Capra <michelecapra83@gmail.com>
2022-03-17 08:21:04 +0100

    KV store point 1 and 2

---
+++ kv/lib/kv/bucket.ex
@@ -0,0 +1,30 @@
+defmodule KV.Bucket do
+  use Agent
+  @doc """
+  Start a new bucket
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Get value
+  """
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Put an element
+  """
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+
+  @doc """
+  delete key from bucket
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
+end
