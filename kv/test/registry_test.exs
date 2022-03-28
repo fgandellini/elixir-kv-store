8a7b16b52dc6429d47b89da6e5be2b103d56376a
Federico Gandellini <fgandellini@gmail.com>
2022-03-28 22:25:34 +0200

    supervisors

--- kv/test/registry_test.exs
+++ kv/test/registry_test.exs
@@ -23,4 +23,12 @@ defmodule KV.RegistryTest do
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
+  test "removes buckets on crash", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    # Stop the bucket with non-normal reason
+    Agent.stop(bucket, :shutdown)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
 end
